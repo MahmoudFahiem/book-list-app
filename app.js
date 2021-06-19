0d5fd9097d1950a8a581009674ea6648855af681
Mahmoud Fahiem <xmahmoud92@gmail.com>
2021-06-19 23:06:32 +0200

    Create remove book item ui method Create show alert ui methodi

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var UIbookAuthor = document.querySelector('#author');
 var UIbookIsbn = document.querySelector('#isbn');
 var UIbookList = document.querySelector('#book-list');
 
-// Book Constructor
+// Book varructor
 
 function Book(title, author, isbn) {
     this.title = title;
@@ -13,7 +13,7 @@ function Book(title, author, isbn) {
     this.isbn = isbn;
 }
 
-// UI Constructor
+// UI varructor
 
 function UI() {};
 
@@ -21,7 +21,7 @@ UI.prototype.addBookToList = function(book) {
     var rowTemp = `
         <tr>
           <td>${book.title}</td>
-          <td>${book.auhor}</td>
+          <td>${book.author}</td>
           <td>${book.isbn}</td>
           <td><a href="#" id="delete">X</a></td>
         </tr>
@@ -30,11 +30,35 @@ UI.prototype.addBookToList = function(book) {
     UIbookList.insertAdjacentHTML('beforeend',rowTemp);
 }
 
+UI.prototype.removeBookItem = function(book) {
+    book.remove();
+}
+
 UI.prototype.clearFields = function() {
     UIbookTitle.value = '';
     UIbookAuthor.value = '';
     UIbookIsbn.value = '';
 }
+
+UI.prototype.showAlert = function(message, className) {
+    var alert = document.querySelector(`.alert.${className}`);
+    if(!alert) {
+        var alertTemp = `
+            <p class="alert ${className}">${message}</p> 
+        `;
+        UIbookForm.insertAdjacentHTML('afterbegin', alertTemp);
+        this.hideAlert();
+    }
+}
+
+UI.prototype.hideAlert = function() {
+    var alert = document.querySelector('.alert');
+    setTimeout(function() {
+        alert.remove();
+    }, '3000');
+}
+
+var ui = new UI();
 
 // Event Listners
 
@@ -43,18 +67,24 @@ UIbookForm.addEventListener('submit', function(e) {
     var bookTitle = UIbookTitle.value,
         bookAuthor = UIbookAuthor.value,
         bookIsbn = UIbookIsbn.value;
-    
-    var book = new Book(bookTitle, bookAuthor, bookIsbn);
-    var ui = new UI();
 
-    ui.addBookToList(book);
-    ui.clearFields();
+    console.log(bookAuthor);
+    if(bookTitle == '' || bookAuthor == '' || bookIsbn == '') {
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+        var book = new Book(bookTitle, bookAuthor, bookIsbn);
+        ui.addBookToList(book);
+        ui.showAlert('Book Item added', 'success');
+        ui.clearFields();
+    }
 })
 
 UIbookList.addEventListener('click', function(e) {
     e.preventDefault();
-    const clickedEl = e.target;
+    var clickedEl = e.target;
     if(clickedEl.id == 'delete') {
-        clickedEl.parentElement.parentElement.remove();
+        var bookItem = clickedEl.parentElement.parentElement;
+        ui.removeBookItem(bookItem);
+        ui.showAlert('Book Item Removed', 'success');
     }
 })
